3b74a59ce82db944a5e44fdbb2b4e7e7aa7e76b6
Gandobilis <lashadeveloper@gmail.com>
2023-11-08 18:23:55 +0400

    chunkArrayInGroups.js

--- basic-algorithm-scripting/chunkArrayInGroups.js
+++ basic-algorithm-scripting/chunkArrayInGroups.js
@@ -16,3 +16,50 @@ function chunkArrayInGroups(arr, size) {
 }
 
 console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+
+function chunkArrayInGroupsSlice(arr, size) {
+    // Break it up.
+    const newArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+        newArr.push(arr.slice(i, i + size));
+    }
+    return newArr;
+}
+
+console.log(chunkArrayInGroupsSlice(["a", "b", "c", "d"], 2));
+
+function chunkArrayInGroupsWhile(arr, size) {
+    // Break it up.
+    const newArr = [];
+    let i = 0;
+
+    while (i < arr.length) {
+        newArr.push(arr.slice(i, i + size));
+        i += size;
+    }
+    return newArr;
+}
+
+console.log(chunkArrayInGroupsWhile(["a", "b", "c", "d"], 2));
+
+function chunkArrayInGroupsSplice(arr, size) {
+    const newArr = [];
+    while (arr.length > 0) {
+        newArr.push(arr.splice(0, size));
+    }
+    return newArr;
+}
+
+console.log(chunkArrayInGroupsSplice(["a", "b", "c", "d"], 2));
+
+function chunkArrayInGroupsRecursive(arr, size) {
+    if (arr.length <= size) {
+        return [arr];
+    } else {
+        return [arr.slice(0, size)].concat(
+            chunkArrayInGroups(arr.slice(size), size)
+        );
+    }
+}
+
+console.log(chunkArrayInGroupsRecursive(["a", "b", "c", "d"], 2));
